67c605637263c65b61a4fa94c4a4b4dce35d9aef
Joseph Goh <jgoh2@uoregon.edu>
2019-10-16 02:07:23 -0700

    added basic headers

---
+++ Project1/command.c
@@ -0,0 +1,13 @@
+/*
+* Description:
+* Project 1 command.c file. Contains function definitions for syscall functionality.
+*
+* Author: Joseph Goh
+*
+* Date: 10/16/2019
+*
+* Notes: 
+*
+*/
+
+#include "command.h"
---
+++ Project1/main.c
@@ -0,0 +1,16 @@
+/*
+* Description:
+* Project 1 main file. Contains pseudo-shell functionality and calls on functions defined in command.c
+*
+* Author: Joseph Goh
+*
+* Date: 10/16/2019
+*
+* Notes: 
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "command.h"
