571ae0732097a7754a4dd6a2405ebec3b7cb75c1
Joseph Goh <jgoh2@uoregon.edu>
2019-10-16 19:18:33 -0700

    Implemented basic main

--- Project1/main.c
+++ Project1/main.c
@@ -13,5 +13,25 @@ TO DO:
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "shell.h"
 #include "command.h"
+
+int main(int argc, char *argv[]) {
+    if ((argc == 2) || (argc > 3)) {
+        printf("Usage: pseudo-shell [-f filename]\n");
+    }
+    else if (argc == 1) {
+        shellInteractive();
+    }
+    else if (argc == 2) {
+        if (strcmp(argv[1], "-f")) {
+            printf("Usage: pseudo-shell [-f filename]\n");
+        }
+        else {
+            shellFile(argv[2]);
+        }
+    }
+
+    return 0;
+}
