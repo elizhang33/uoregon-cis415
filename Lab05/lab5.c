93be19bca2ca02612eeecf81741dbe42718ff333
Joseph Goh <jgoh2@uoregon.edu>
2019-10-31 01:32:57 -0700

    Implemented process forking and began work on signal handling

--- Lab05/lab5.c
+++ Lab05/lab5.c
@@ -11,24 +11,60 @@
 */
 
 /*-------------------------Preprocessor Directives---------------------------*/
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 /*---------------------------------------------------------------------------*/
 
+/*----------------------------Signal Handler---------------------------------*/
+void usr_handler(int signum) {
+	if (signum == SIGUSR1) {
+		printf("Child Process: %d - Received signal: SIGUSR1\n", getpid());
+	}
+}
+/*---------------------------------------------------------------------------*/
+
 /*-----------------------------Program Main----------------------------------*/
-int	 main()
+int	main()
 {
 	//variable definitions
 	pid_t  pid, w;
 	int wstatus, i;
 	i=0;
-	
-	pid = fork();
+
+	// Define SIGUSR1 behavior before we do anything else
+	struct sigaction ;
+	sigaction(SIGUSR1);
+
+	// Have the pool be a malloc'ed pointer so we don't copy unnecessary data
+	pid_t* pool = malloc(sizeof(pid_t) * 5);
+
+	int j;
+	for (j = 0; j < 5; ++j) {
+		pid = fork();
+		// If pid == 0, I am a child process, so don't fork anymore and break
+		if (pid == 0) {
+			break;
+		}
+		// If pid == -1, something went wrong so we might as well abort
+		else if (pid == -1) {
+			printf("DEBUG: Apparently we couldn't fork for some reason...\n");
+			exit(-1);
+		}
+		// Otherwise, I am the parent and will record the returned child pid in my pool
+		else if (pid > 0) {
+			pool[j] = pid;
+		}
+	}
+
+
+	// We want to sigwait before we start this block if we recieve SIGUSR1 
 	if (pid == 0) {
 		printf("	Child Process: %i - Starting infinite loop...\n", getpid());
 		while(1) {
