1c3a0d14238dde6e258235e01946f6bbc897b95c
Joseph Goh <jgoh2@uoregon.edu>
2019-11-07 11:19:59 -0800

    I think it's done but who knows!

--- Lab06/lab6.c
+++ Lab06/lab6.c
@@ -1,15 +1,18 @@
 /*-----------------------------------------------------------------------------
-File name: Lab5.c
+File name: Lab6.c
 Description: A simple lab showing the use of signals.
 Author: Jared Hall
+        Joseph Goh
 -----------------------------------------------------------------------------*/
 
 /*---------------------------Preprocessor Directives-------------------------*/
+#define _GNU_SOURCE
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <time.h>
 /*---------------------------------------------------------------------------*/
 
 /*---------------------------------Functions---------------------------------*/
@@ -27,20 +30,32 @@ int main(void)
     if(pid < 0) {
         perror("Error! could not create a new process.\n");
         exit(EXIT_FAILURE);
-    } else if(pid == 0) {
+    }
+    else if(pid == 0) {
+        int mypid = getpid();
+        int ctr = 0;
         //This code runs in the child process only
-	printf("    Child process: %d - Starting...\n", getpid());
-	//Add your while loop here
-	exit(EXIT_SUCCESS);
-
-    } else if(pid > 0) {
+	    printf("    Child process: %d - Starting...\n", mypid);
+	    //Add your while loop here
+        while(ctr < 10) {
+            sleep(1);
+            ctr++;
+            printf("Child process %d- Still alive after for %d seconds\n", mypid, ctr);
+        }
+	    exit(EXIT_SUCCESS);
+    }
+    else if(pid > 0) {
         //This code runs in the parent process only
-	printf("Parent process: %d - Sending signals to child...\n", getpid());
-	//Add code to send your signals  in a loop here
-	
-	printf("Parent process: %d - Waiting for child to complete...\n", getpid());
-	w = waitpid(pid, &wstatus, 0);
-	printf("Parent process: %d - Finished\n", getpid());
+	    printf("Parent process: %d - Sending signals to child...\n", getpid());
+	    //Add code to send your signals  in a loop here
+        sleep(1);
+        kill(pid, SIGSTOP);
+        sleep(3);
+        kill(pid, SIGCONT);
+        sleep(1);
+	    printf("Parent process: %d - Waiting for child to complete...\n", getpid());
+	    w = waitpid(pid, &wstatus, 0);
+	    printf("Parent process: %d - Finished\n", getpid());
     }
     
     //exit out of program
