8f437663f09a218fd24a399d515bd429a404f86f
Joseph Goh <jgoh2@uoregon.edu>
2019-11-10 04:16:38 -0800

    Edited test executables to take less long to finish

--- Project2/test/iobound.c
+++ Project2/test/iobound.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    int i, j, minutes = 1, number = 600000000;
+    int i, j, minutes = 1, number = 60000000;
     char b[1000];
     int fd = open("/dev/null", O_WRONLY);
     char name[128];
