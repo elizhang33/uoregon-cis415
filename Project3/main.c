3c552f26b316d268b1a523e3cc16a8a0c0ee7c45
Joseph Goh <jgoh2@uoregon.edu>
2019-12-08 10:28:00 -0800

    Added skeleton

---
+++ Project3/main.c
@@ -0,0 +1,18 @@
+/*
+Description:
+    CIS 415 Project 3 InstaQuack!
+    main.c
+
+Author: Joseph Goh
+Last updated: 2019/12/08
+
+Notes:
+    N/A
+
+TO DO:
+    N/A
+*/
+
+int main(int argc, char *argv[]) {
+    
+}
