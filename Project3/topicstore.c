7fcfae8dc5ddde3c5cca3b9f23efec4fbd2abcc5
Joseph Goh <jgoh2@uoregon.edu>
2019-12-08 05:09:24 -0800

    Moved part 1 topic store functionality to topicstore.h/c

---
+++ Project3/topicstore.c
@@ -0,0 +1,231 @@
+/*
+Description:
+    CIS 415 Project 3 InstaQuack!
+    topicstore.c
+
+Author: Joseph Goh
+Last updated: 2019/12/08
+
+Notes:
+    N/A
+
+TO DO:
+    1. Do Part 2
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include "topicstore.h"
+
+int buildTQ(char *name, topicQueue *newQueue) {
+    if (strlen(name) >= NAMESIZE) {
+        printf("ERROR: Failed to build topicQueue. Name too long.\n");
+        return 0;
+    }
+    strcpy(newQueue->name, name);
+    // Queue starts out empty
+    newQueue->entryCtr = 1;
+    newQueue->head = -1;
+    newQueue->tail = 0;
+    pthread_mutex_init(&newQueue->lock, PTHREAD_MUTEX_ERRORCHECK_NP);
+
+    return 1;
+}
+
+int enqueue(topicEntry *newEntry, topicQueue *TQ) {
+    int is_full = 0;
+    int ret;
+
+    // mutex lock check
+    int result;
+    while (1) {
+        pthread_mutex_trylock(&TQ->lock);
+        if (result == 0) {
+            break;
+        }
+        else {
+            sched_yield();
+        }
+    }
+    
+    int head = TQ->head;
+    int tail = TQ->tail;
+
+    // Check whether queue is full and set appropriate tail value
+    // Case 1: topicQueue is empty
+    if (head == -1) {
+        TQ->head = 0;
+        TQ->tail = 0;
+    }
+    // Case 2: topicQueue is NOT full (incrementing the tail wouldn't overwrite the head)
+    else if ((tail + 1) % MAXENTRIES == head) {
+        if (tail + 1 == MAXENTRIES) {
+            TQ->tail = 0;
+        }
+        else {
+            TQ->tail++;
+        }
+    }
+    // Case 3: topicQueue is full (since it's not empty or not-full)
+    else {
+        is_full = 1;
+    }
+
+    // enqueue the topic entry into buffer at updated tail index unless the queue is full
+    if (is_full) {
+        ret = 0;
+    }
+    else {
+        TQ->buffer[TQ->tail].entryNum = TQ->entryCtr;
+        TQ->entryCtr++;
+        gettimeofday(&TQ->buffer[TQ->tail].timeStamp, NULL);
+        TQ->buffer[TQ->tail].pubID = newEntry->pubID;
+        strcpy(TQ->buffer[TQ->tail].photoURL, newEntry->photoURL);
+        strcpy(TQ->buffer[TQ->tail].photoCaption, newEntry->photoCaption);
+
+        ret = 1;
+    }
+
+    pthread_mutex_unlock(&TQ->lock);
+    return ret;
+}
+
+int getEntry(int lastEntry, topicQueue *TQ, topicEntry *entry) {
+    int found = 0;
+    int numEntries, index, i, ret;
+
+    // mutex lock check
+    int result;
+    while (1) {
+        pthread_mutex_trylock(&TQ->lock);
+        if (result == 0) {
+            break;
+        }
+        else {
+            sched_yield();
+        }
+    }
+
+    if (TQ->head <= TQ->tail) {
+        numEntries = TQ->tail - TQ->head + 1;
+    }
+    else {
+        numEntries = TQ->tail - TQ->head + MAXENTRIES + 1;
+    }
+    
+    // Case 1: topicQueue is empty
+    if (TQ->head == -1) {
+        ret = 0;
+    }
+    else {
+        for (i = 0; i < numEntries; i++) {
+            index = (TQ->head + i) % MAXENTRIES;
+            if (TQ->buffer[index].entryNum == lastEntry + 1) {
+                found = 1;
+                break;
+            }
+        }
+
+        // Case 2: The next entry to get is found in the topicQueue
+        if (found) {
+            entry->entryNum = TQ->buffer[index].entryNum;
+            entry->timeStamp = TQ->buffer[index].timeStamp;
+            entry->pubID = TQ->buffer[index].pubID;
+            strcpy(entry->photoURL, TQ->buffer[index].photoURL);
+            strcpy(entry->photoCaption, TQ->buffer[index].photoCaption);
+
+            ret = 1;
+        }
+        // Case 3: topicQueue is not empty but next entry is not found...
+        else {
+            for (i = 0; i < numEntries; i++) {
+                index = (TQ->head + i) % MAXENTRIES;
+                if (TQ->buffer[index].entryNum > lastEntry + 1) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            // Case 3-ii: The lastEntry + 1 must have been dequeued but we have an entry added after that
+            if (found) {
+                entry->entryNum = TQ->buffer[index].entryNum;
+                entry->timeStamp = TQ->buffer[index].timeStamp;
+                entry->pubID = TQ->buffer[index].pubID;
+                strcpy(entry->photoURL, TQ->buffer[index].photoURL);
+                strcpy(entry->photoCaption, TQ->buffer[index].photoCaption);
+
+                ret = TQ->buffer[index].entryNum;
+            }
+            // Case 3-i: Our desired entry is yet to arrive
+            else {
+                ret = 0;
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&TQ->lock);
+    return ret;
+}
+
+int dequeue(topicQueue *TQ, suseconds_t delta) {
+    struct timeval currenttime, age;
+    int numEntries, index, i, ret;
+
+    // mutex lock check
+    int result;
+    while (1) {
+        pthread_mutex_trylock(&TQ->lock);
+        if (result == 0) {
+            break;
+        }
+        else {
+            sched_yield();
+        }
+    }
+
+    if (TQ->head <= TQ->tail) {
+        numEntries = TQ->tail - TQ->head + 1;
+    }
+    else {
+        numEntries = TQ->tail - TQ->head + MAXENTRIES + 1;
+    }
+    
+    // If topicQueue is empty, we don't dequeue anything
+    if (TQ->head == -1) {
+        ret = 0;
+    }
+    else {
+        // Parsing the entries from oldest to newest...
+        for (i = 0; i < numEntries; i++) {
+            index = (TQ->head + i) % MAXENTRIES;
+            timersub(&currenttime, TQ->buffer[index].timeStamp, age);
+            // If we hit an entry that's not old enough, all entries after that aren't either so break
+            if (age.tv_usec < delta) {
+                break;
+            }
+        }
+
+        // Case 1: The head (and every other entry by extension) is too new to be dequeued
+        if (i == 0) {
+            ret = 0;
+        }
+        // Case 2: Every entry is old enough to dequeue (since the for loop iterated past the tail)
+        else if (i == numEntries) {
+            TQ->head = -1;
+            TQ->tail = 0;
+
+            ret = 1;
+        }
+        // Case 3: Dequeue up to [index - 1] by updating the head to index
+        else {
+            TQ->head = index;
+
+            ret = 1;
+        }
+    }
+
+    pthread_mutex_unlock(&TQ->lock);
+    return ret;
+}
---
+++ Project3/topicstore.h
@@ -0,0 +1,55 @@
+/*
+Description:
+    CIS 415 Project 3 InstaQuack! Part 1
+    topicstore.h
+
+Author: Joseph Goh
+Last updated: 2019/12/08
+
+Notes:
+    N/A
+
+TO DO:
+    N/A
+*/
+
+#ifndef QUACKER_H_
+#define QUACKER_H_
+
+#include <pthread.h>
+#include <sys/time.h>
+
+#define URLSIZE 281
+#define CAPSIZE 281
+#define NAMESIZE 20
+#define MAXENTRIES 100
+#define MAXTOPICS 20
+
+typedef struct topicEntry {
+    int entryNum;
+    struct timeval timeStamp;
+    int pubID;
+    char photoURL[URLSIZE]; // URL to photo
+    char photoCaption[CAPSIZE]; // photo caption
+} topicEntry;
+
+// Each instance of topicQueue stores the topicEntries for a given topic 
+typedef struct topicQueue {
+    char name[NAMESIZE];
+    int entryCtr;
+    topicEntry buffer[MAXENTRIES];
+    // Set head to -1 (and tail to 0) to signify an empty queue
+    int head;
+    int tail;
+    pthread_mutex_t lock;
+} topicQueue;
+
+int buildTQ(char *name, topicQueue *newQueue);
+
+int enqueue(topicEntry *newEntry, topicQueue *TQ);
+
+int getEntry(int lastEntry, topicQueue *TQ, topicEntry *entry);
+
+int dequeue(topicQueue *TQ, suseconds_t delta);
+
+#endif
